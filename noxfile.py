69cd4acb8da419452f5405002c6f9f520a1025a0
rterluun <128294216+rterluun@users.noreply.github.com>
2024-12-21 13:32:03 +0100

    Add initial project structure with README, source code, tests, and CI configuration (#2)
    Co-authored-by: Robert ter Luun <rterluun@ilionx.com>

---
+++ noxfile.py
@@ -0,0 +1,25 @@
+import nox
+
+
+@nox.session
+def tests(session):
+    session.install("coverage==7.6.9")
+    session.install("pytest==8.3.4")
+
+    session.run("coverage", "run", "--source", "src", "-m", "pytest", "tests")
+    session.run("coverage", "report", "-m")
+
+
+@nox.session
+def lint(session):
+    session.install("black==24.10.0")
+    session.install("flake8==7.1.1")
+    session.install("isort==5.13.2")
+    session.install("mypy==1.14.0")
+    session.install("yamllint==1.35.1")
+
+    session.run("black", "--check", ".")
+    session.run("flake8")
+    session.run("isort", "--check", ".")
+    session.run("mypy", ".")
+    session.run("yamllint", "--no-warnings", ".")
