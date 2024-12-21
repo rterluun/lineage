3aae126c370bab88fe87722c74e174864284f832
Robert ter Luun <rterluun@ilionx.com>
2024-12-21 17:47:00 +0100

    Refactor test_filter_copy_activities to improve readability and add output validation

--- tests/test_search.py
+++ tests/test_search.py
@@ -3,13 +3,16 @@ from search.pipeline import filter_copy_activities
 
 def test_filter_copy_activities(adf_copy_activity):
 
+    filtered_copy_activities = filter_copy_activities(
+        copy_activities=adf_copy_activity,
+        filter="outputs",
+        filter_value="rawFolderPath",
+    )
+
+    assert len(filtered_copy_activities) == 1
     assert (
-        len(
-            filter_copy_activities(
-                copy_activities=adf_copy_activity,
-                filter="outputs",
-                filter_value="rawFolderPath",
-            )
-        )
-        == 1
+        filtered_copy_activities[0].outputs[0]["parameters"]["pFolder"][
+            "value"
+        ]  # noqa: E501
+        == "@parameters('rawFolderPath')"
     )
