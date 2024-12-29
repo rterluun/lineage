2587eb39ee3e043ff87dc23c8fc9af428f78553d
Robert ter Luun <rterluun@ilionx.com>
2024-12-29 09:17:10 +0100

    Refactor Adf class to use 'data' attribute instead of 'dataclass' for improved clarity and consistency

--- src/lineage/adf.py
+++ src/lineage/adf.py
@@ -12,13 +12,13 @@ LOGGER = getLogger(__name__)
 class Adf:
     def __init__(
         self,
-        dataclass: Union[
+        data: Union[
             dataclasses.Pipeline,
             dataclasses.Dataset,
             dataclasses.LinkedService,
         ],
     ):
-        self.dataclass = dataclass
+        self.data = data
 
     @classmethod
     def from_jsonfile(cls, file_path: str):
@@ -29,58 +29,61 @@ class Adf:
         )
 
         if cls.__name__ == "Pipeline" and json_data:
-            pipeline_dataclass = dataclasses.Pipeline(
-                name=json_data.get("name", None),
-                file_path=file_path,
-                json_data=json_data,
+            return cls(
+                data=dataclasses.Pipeline(
+                    name=json_data.get("name", None),
+                    file_path=file_path,
+                    json_data=json_data,
+                )
             )
-            return cls(dataclass=pipeline_dataclass)
 
         if cls.__name__ == "Dataset" and json_data:
-            dataset_dataclass = dataclasses.Dataset(
-                name=json_data.get("name", None),
-                file_path=file_path,
-                json_data=json_data,
-                linked_service_name=json_data.get("properties", {})
-                .get("linkedServiceName", {})
-                .get("referenceName", None),
+            return cls(
+                data=dataclasses.Dataset(
+                    name=json_data.get("name", None),
+                    file_path=file_path,
+                    json_data=json_data,
+                    linked_service_name=json_data.get("properties", {})
+                    .get("linkedServiceName", {})
+                    .get("referenceName", None),
+                )
             )
-            return cls(dataclass=dataset_dataclass)
 
         if cls.__name__ == "LinkedService" and json_data:
-            linkedservice_dataclass = dataclasses.LinkedService(
-                name=json_data.get("name", None),
-                file_path=file_path,
-                json_data=json_data,
+            return cls(
+                data=dataclasses.LinkedService(
+                    name=json_data.get("name", None),
+                    file_path=file_path,
+                    json_data=json_data,
+                )
             )
-            return cls(dataclass=linkedservice_dataclass)
 
 
 class Pipeline(Adf):
     def __init__(
         self,
-        dataclass: dataclasses.Pipeline = dataclasses.Pipeline(
+        data: dataclasses.Pipeline = dataclasses.Pipeline(
             name=None,
             file_path=None,
             json_data=None,
         ),
         logger: Logger = LOGGER,
     ):
-        super().__init__(dataclass=dataclass)
-        self.pipeline: dataclasses.Pipeline = dataclass
+        super().__init__(data=data)
         self.logger = logger
-        self.copy_activities: list[dataclasses.CopyActivity] = find_copy_activities(
-            pipeline=dataclass
-        )
+        self.copy_activities: list[dataclasses.CopyActivity] = []
+
+        if isinstance(self.data, dataclasses.Pipeline):
+            self.copy_activities = find_copy_activities(pipeline=self.data)
 
     def lineage(
         self,
         datasets_dir_path: str,
         linked_services_dir_path: str,
     ):
-        if self.pipeline.name:
+        if self.data.name:
             log(
-                pipeline_name=self.pipeline.name,
+                pipeline_name=self.data.name,
                 copy_activities=self.copy_activities,
                 datasets_dir_path=datasets_dir_path,
                 linked_services_dir_path=linked_services_dir_path,
@@ -91,25 +94,23 @@ class Pipeline(Adf):
 class Dataset(Adf):
     def __init__(
         self,
-        dataclass: dataclasses.Dataset = dataclasses.Dataset(
+        data: dataclasses.Dataset = dataclasses.Dataset(
             name=None,
             file_path=None,
             json_data=None,
             linked_service_name=None,
         ),
     ):
-        super().__init__(dataclass=dataclass)
-        self.dataset: dataclasses.Dataset = dataclass
+        super().__init__(data=data)
 
 
 class LinkedService(Adf):
     def __init__(
         self,
-        dataclass: dataclasses.LinkedService = dataclasses.LinkedService(
+        data: dataclasses.LinkedService = dataclasses.LinkedService(
             name=None,
             file_path=None,
             json_data=None,
         ),
     ):
-        super().__init__(dataclass=dataclass)
-        self.linkedservice: dataclasses.LinkedService = dataclass
+        super().__init__(data=data)
--- tests/test_adf.py
+++ tests/test_adf.py
@@ -4,24 +4,24 @@ from lineage.adf import Dataset, LinkedService, Pipeline
 
 def test_pipeline_from_jsonfile(adf_pipeline: Pipeline):
     pipeline = Pipeline().from_jsonfile(file_path="tests/data/pipeline.json")
-    assert pipeline.pipeline == adf_pipeline
+    assert pipeline.data == adf_pipeline
 
 
 def test_pipeline_copy_activities(
     adf_pipeline: dataclasses.Pipeline,
     adf_copy_activity: dataclasses.CopyActivity,
 ):
-    pipeline = Pipeline(dataclass=adf_pipeline)
+    pipeline = Pipeline(data=adf_pipeline)
     assert pipeline.copy_activities == adf_copy_activity
 
 
 def test_dataset_from_jsonfile(adf_dataset: Dataset):
     dataset = Dataset().from_jsonfile(file_path="tests/data/dataset.json")
-    assert dataset.dataset == adf_dataset
+    assert dataset.data == adf_dataset
 
 
 def test_linkedservice_from_jsonfile(adf_linkedservice: dataclasses.LinkedService):
     linkedservice = LinkedService().from_jsonfile(
         file_path="tests/data/linkedservice.json"
     )
-    assert linkedservice.linkedservice == adf_linkedservice
+    assert linkedservice.data == adf_linkedservice
